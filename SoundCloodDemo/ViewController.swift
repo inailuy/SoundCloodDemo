4f4331f8e143ce54ae0d151ce0139f7ce4050388
iNailuY <inailuy@me.com>
2016-04-29 00:38:31 -0500

    init commit

---
+++ SoundCloodDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoundCloodDemo
+//
+//  Created by inailuy on 4/29/16.
+//  Copyright © 2016 inailuy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
